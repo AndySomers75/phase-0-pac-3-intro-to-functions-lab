f4010e1eeed8a1f51ca89476584f56b8f7d859a4
Andrew <andrew@pureaquasprings.com>
2022-03-02 10:58:17 +1100

    commit tree 2d0908347b5231b7aa2e2019a4f41d5ebe4b9e50

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-function shout(string) {
-    // todo
+function shout(HELLO) {
+  return HELLO.toUpperCase();
+}
+
+function whisper(hello) {
+  return hello.toLowerCase();
+}
+function logShout(HELLO) {
+  console.log(HELLO.toUpperCase());
+}
+
+function logWhisper(hello){
+  console.log(hello.toLowerCase());
   }
-  
+
+function sayHiToHeadphonedRoommate(str){
+if(str === str.toLowerCase()){
+  return "I can't hear you!"
+}else if(str === str.toUpperCase()){
+  return "YES INDEED!"
+}else if(str === "Let's have dinner together!"){
+  return "I would love to!";
+}
+}
